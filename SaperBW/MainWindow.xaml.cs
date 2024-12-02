ee4eba91fd4ff7c29ee5b6a5930c7ac67b6a3174
Bartosz Wronowski <bartix320@gmail.com>
2024-12-02 15:49:36 +0100

    wiecej w teorii dziala

--- SaperBW/MainWindow.xaml.cs
+++ SaperBW/MainWindow.xaml.cs
@@ -18,10 +18,12 @@ public partial class MainWindow : Window
 {
     private int rozmiar = 10;
     private int iloscBomb = 10;
+    private int iloscFlag;
     public MainWindow()
     {
         InitializeComponent();
-
+        iloscFlag = iloscBomb;
+        
         for (int i = 0; i < 10; i++)
         {
             plansza.RowDefinitions.Add(new RowDefinition());
@@ -37,15 +39,86 @@ public partial class MainWindow : Window
                 Background = Brushes.Black,
                 Foreground = Brushes.LightGray
             };
+            przycisk.Click += PrzyciskOnClick;
+            przycisk.MouseRightButtonDown += PrzyciskOnPrawyKlik;
             Grid.SetRow(przycisk, i);
             Grid.SetColumn(przycisk, j);
             plansza.Children.Add(przycisk);
         }
         
         RozstawBomby(iloscBomb);
-        PokazPlansze();
+        // PokazPlansze();
     }
 
+    private void PrzyciskOnPrawyKlik(object sender, MouseButtonEventArgs e)
+    {
+        Przycisk przycisk = (Przycisk)sender;
+        int x = Grid.GetRow(przycisk);
+        int y = Grid.GetColumn(przycisk);
+        Flaga(x, y);
+    }
+
+    private void PrzyciskOnClick(object sender, RoutedEventArgs e)
+    {
+        Przycisk przycisk = (Przycisk)sender;
+        int x = Grid.GetRow(przycisk);
+        int y = Grid.GetColumn(przycisk);
+        OdkryjPrzycisk(x, y);
+    }
+
+    private void OdkryjPrzycisk(int x, int y)
+    {
+        Przycisk przycisk = WyszukajPrzycisk(x, y);
+        if(przycisk.Content!=null)
+            return;
+        if (przycisk.Wartosc == 10)
+        {
+            MessageBox.Show("Przegrales");
+            PokazPlansze();
+        }
+        else
+        {
+            przycisk.Content = przycisk.Wartosc;
+            if (przycisk.Wartosc != 0)
+                return;
+            for (int i = x - 1; i <= x + 1; i++)
+            for (int j = y - 1; j <= y + 1; j++)
+            {
+                if (i >= 0 && i < rozmiar && j >= 0 && j < rozmiar)
+                {
+                    Przycisk sasiad = WyszukajPrzycisk(i, j);
+                    if (sasiad.Content == null)
+                    {
+                        sasiad.Content = sasiad.Wartosc;
+                        if (sasiad.Wartosc == 0)
+                            OdkryjPrzycisk(i,j); 
+                    }
+                }
+            }
+        }
+    }
+
+    private void Flaga(int x, int y)
+    {
+        Przycisk przycisk = WyszukajPrzycisk(x, y);
+        if (przycisk.Content == null)
+        {
+            if (iloscFlag > 0)
+            {
+                przycisk.Content = "🏳️‍⚧️️";
+                iloscFlag--;
+            }
+        }
+        else
+        {
+            if (przycisk.Content.ToString() == "🏳️‍⚧️")
+            {
+                przycisk.Content = null;
+                iloscFlag++;
+            }
+        }
+    }
+    
     private Przycisk WyszukajPrzycisk(int x, int y)
     {
         var przycisk = (Przycisk)plansza.Children
@@ -65,10 +138,27 @@ public partial class MainWindow : Window
             {
                 przycisk.Wartosc = 10;
                 ilosc--;
+                ZliczBomby(x,y);
             }
         }
     }
 
+    private void ZliczBomby(int x, int y)
+    {
+        for(int i = x - 1;i<=x+1;i++) 
+            for(int j=y-1;j<=y+1;j++)
+            {
+                if (i >= 0 && i < rozmiar && j >= 0 && j < rozmiar)
+                {
+                    Przycisk przycisk = WyszukajPrzycisk(i, j);
+                    if (przycisk.Wartosc != 10)
+                    {
+                        przycisk.Wartosc++;
+                    }
+                }
+            }
+    }
+    
     private void PokazPlansze()
     {
         plansza.Children.Cast<Przycisk>().ToList().ForEach(przycisk =>
